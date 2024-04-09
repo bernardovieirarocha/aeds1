71dcef80b934f21ec89795f8851b6168eb20a1df
upwardweb <upwardweb53@gmail.com>
2024-04-09 20:51:38 -0300

    Formatacao e nome de arquivos consertados:

--- Ex_Lista/Entrada_SaidaDeDados/ex20.c
+++ Ex_Lista/Entrada_SaidaDeDados/ex20.c
@@ -1,57 +1,52 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 int main(void) {
-	
-	char letraopt;
-	float lado = 0;
-	float res = 0;
-	float base, altura;
-	float raio;
-	float base_maior, base_menor;
-	
-	printf("Escolha a opcao que deseja calcular (a-d): ");
-	scanf(" %c%*c",&letraopt);
-	
-	while (letraopt != 's' ) {
-		switch (letraopt) {
-			case 'a':
-				lado = 0;
-				printf("Digite o valor do lado do quadrado: ");	
-				scanf(" %f%*c", &lado);
-				res = lado * lado;
-				printf("A area do quadrado eh: %2.f", res);	
-				break;
-			case 'b':
-				printf("Digite o valor da base e da altura: ");	
-				scanf(" %f %f%*c", &base, &altura);
-				res = (base * altura) / 2;
-				printf("A area do triangulo eh: %2.f", res);
-				break;
-			case 'c':
-				printf("Digite o valor do raio: ");
-				scanf(" %f%*c", &raio);
-				res = M_PI * pow(raio, 2);
-				printf("A area do circulo eh: %2.f", res);	
-				break;
-		
-			case 'd':
-				printf("Digite o valor da base maior, da base menor e da altura: ");
-				scanf(" %f %f %f%*c", &base_maior, &base_menor, &altura);
-				res = ((base_maior + base_menor) * altura )/2;
-				printf("A area do trapezio eh: %2.f", res);	
-				break;
-			default:
-				printf("Essa opcao nao existe!\n");
-		
-		
-		}
-		printf("\nEscolha a opcao que deseja calcular (a-d): ");
-		scanf(" %c%*c",&letraopt);
-	}
-	
-	
-	
-	return 0;	
+
+  char letraopt;
+  float lado = 0;
+  float res = 0;
+  float base, altura;
+  float raio;
+  float base_maior, base_menor;
+
+  printf("Escolha a opcao que deseja calcular (a-d): ");
+  scanf(" %c%*c", &letraopt);
+
+  while (letraopt != 's') {
+    switch (letraopt) {
+    case 'a':
+      lado = 0;
+      printf("Digite o valor do lado do quadrado: ");
+      scanf(" %f%*c", &lado);
+      res = lado * lado;
+      printf("A area do quadrado eh: %2.f", res);
+      break;
+    case 'b':
+      printf("Digite o valor da base e da altura: ");
+      scanf(" %f %f%*c", &base, &altura);
+      res = (base * altura) / 2;
+      printf("A area do triangulo eh: %2.f", res);
+      break;
+    case 'c':
+      printf("Digite o valor do raio: ");
+      scanf(" %f%*c", &raio);
+      res = M_PI * pow(raio, 2);
+      printf("A area do circulo eh: %2.f", res);
+      break;
+
+    case 'd':
+      printf("Digite o valor da base maior, da base menor e da altura: ");
+      scanf(" %f %f %f%*c", &base_maior, &base_menor, &altura);
+      res = ((base_maior + base_menor) * altura) / 2;
+      printf("A area do trapezio eh: %2.f", res);
+      break;
+    default:
+      printf("Essa opcao nao existe!\n");
+    }
+    printf("\nEscolha a opcao que deseja calcular (a-d): ");
+    scanf(" %c%*c", &letraopt);
+  }
+
+  return 0;
 }
